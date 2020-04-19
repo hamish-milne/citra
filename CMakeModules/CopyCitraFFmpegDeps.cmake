670c0ca684ebfee6ada00d53fe4c5a9dc93392de
Hamish Milne <hamishmilne83@gmail.com>
2020-04-19 19:23:30 +0100

    Explicitly set the DLL destination

--- CMakeModules/CopyCitraFFmpegDeps.cmake
+++ CMakeModules/CopyCitraFFmpegDeps.cmake
@@ -1,6 +1,5 @@
 function(copy_citra_FFmpeg_deps target_dir)
     include(WindowsCopyFiles)
-    set(DLL_DEST "${CMAKE_BINARY_DIR}/bin/$<CONFIG>/")
     windows_copy_files(${target_dir} ${FFMPEG_DIR}/bin ${DLL_DEST}
         avcodec*.dll
         avformat*.dll
